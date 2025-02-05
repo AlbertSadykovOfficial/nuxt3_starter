a53c09f9e2424ace4989d6f1df6a259dc2246845
ALBERT SADYKOV <albertsadykov@ro.ru>
2025-02-05 14:40:01 +0300

    debounce util added

---
+++ app/utils/debounce.ts
@@ -0,0 +1,13 @@
+/** @global */
+export const debounce = <T extends unknown[]>(
+  callback: (...args: T) => void,
+  delay: number,
+) => {
+  let timerId: number
+  return function f(...args: T) {
+    if (timerId) clearTimeout(timerId)
+    timerId = window.setTimeout(() => {
+      callback(...args)
+    }, delay)
+  }
+}
